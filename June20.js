10b0a04838660e708f66598966639a708363ed24
G.Jagan <ajagan913@gmail.com>
2026-06-24 11:42:29 +0530

    origin

---
+++ June20.js
@@ -0,0 +1,13 @@
+fetch("https://jsonplaceholder.typicode.com/posts",{
+        method:"POST",
+        headers:{
+            "Content Type": "application/json"   
+}
+    body: JSON.stringify({
+        title: "My post",
+        body:" Learning fetch",
+        Userid: 1
+    })
+})
+.then(response => response.json())
+.then(data => console.log(data))
